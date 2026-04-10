9590c370634e29cb581a45153c00bc85a5ce3284
nikita69tv <71224421+nikita69tv@users.noreply.github.com>
2026-04-10 15:37:53 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,71 +1,71 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# Название файла: diy-part2.sh
+# Описание: Скрипт для тонкой настройки пакетов после обновления фидов
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# --- 1. СЕТЕВЫЕ НАСТРОЙКИ ---
+# Изменение дефолтного IP роутера (раскомментируй и поменяй, если нужно)
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# Обновляем фиды
-#./scripts/feeds update -a
-#./scripts/feeds install -a
-
-# Выбираем пакеты для сборки (эквивалент выбора в menuconfig)
-echo "CONFIG_PACKAGE_luci-app-podkop=y" >> .config
-echo "CONFIG_PACKAGE_sing-box=y" >> .config
-echo "CONFIG_PACKAGE_dnsmasq-full=y" >> .config
-echo "CONFIG_PACKAGE_dnsmasq=n" >> .config
-# Выбираем пакеты AmneziaWG
-echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
-echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
-echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-amneziawg-ru=y" >> .config
-
-# Если хочешь, чтобы Подкоп тоже был сразу в прошивке:
-echo "CONFIG_PACKAGE_podkop=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-podkop=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-podkop-ru=y" >> .config
-
-# 1. Замена базовых пакетов на расширенные (dnsmasq и wpad)
+# --- 2. ЗАМЕНА СТАНДАРТНЫХ КОМПОНЕНТОВ ---
+# Удаляем базовый dnsmasq и ставим полную версию (нужна для многих VPN решений)
 echo "CONFIG_PACKAGE_dnsmasq=n" >> .config
 echo "CONFIG_PACKAGE_dnsmasq-full=y" >> .config
+
+# Удаляем базовый wpad и ставим версию с поддержкой OpenSSL (для продвинутого Wi-Fi)
 echo "CONFIG_PACKAGE_wpad-basic-mbedtls=n" >> .config
 echo "CONFIG_PACKAGE_wpad-openssl=y" >> .config
 
-# 2. Основной софт (Утилиты и системные компоненты)
-for pkg in luci luci-i18n-base-ru luci-i18n-firewall-ru luci-i18n-package-manager-ru \
-luci-ssl sing-box curl jq ca-bundle nano mc btop htop netdata openssh-sftp-server \
-luci-app-sqm luci-i18n-sqm-ru sqm-scripts tc-tiny \
-luci-app-upnp luci-i18n-upnp-ru luci-app-watchcat luci-i18n-watchcat-ru watchcat \
+# --- 3. ИНТЕРФЕЙС И ЛОКАЛИЗАЦИЯ ---
+# Базовая LuCI, русский язык и поддержка SSL для веб-морды
+for pkg in luci luci-ssl luci-i18n-base-ru luci-i18n-firewall-ru luci-i18n-package-manager-ru; do
+    echo "CONFIG_PACKAGE_$pkg=y" >> .config
+done
+
+# --- 4. СИСТЕМНЫЕ УТИЛИТЫ ---
+# Набор "джентльмена": файловые менеджеры, редакторы и системные мониторы
+for pkg in nano mc btop htop curl jq ca-bundle openssh-sftp-server; do
+    echo "CONFIG_PACKAGE_$pkg=y" >> .config
+done
+
+# --- 5. МОНИТОРИНГ И СТАТИСТИКА ---
+# Статистика в реальном времени (Netdata) и графики (Statistics/Collectd)
+echo "CONFIG_PACKAGE_netdata=y" >> .config
+
+for pkg in luci-app-statistics luci-i18n-statistics-ru collectd; do
+    echo "CONFIG_PACKAGE_$pkg=y" >> .config
+done
+
+# Модули для сбора данных collectd (процессор, память, сеть, wifi)
+for mod in conntrack cpu cpufreq entropy interface iwinfo load memory network ping rrdtool thermal uptime; do
+    echo "CONFIG_PACKAGE_collectd-mod-$mod=y" >> .config
+done
+
+# Экспортеры для Prometheus (если используешь внешнюю панель мониторинга)
+for prom in lua lua-nat_traffic lua-netstat lua-openwrt lua-wifi; do
+    echo "CONFIG_PACKAGE_prometheus-node-exporter-$prom=y" >> .config
+done
+
+# --- 6. СЕТЕВЫЕ СЕРВИСЫ (SQM, UPNP, WATCHCAT) ---
+# SQM (борьба с лагами), UPnP и автоматический ребутер (Watchcat)
+for pkg in luci-app-sqm luci-i18n-sqm-ru sqm-scripts tc-tiny \
+luci-app-upnp luci-i18n-upnp-ru \
+luci-app-watchcat luci-i18n-watchcat-ru watchcat \
 luci-app-nlbwmon luci-i18n-nlbwmon-ru nlbwmon; do
     echo "CONFIG_PACKAGE_$pkg=y" >> .config
 done
 
-# 3. Мониторинг (Statistics и Prometheus)
-for pkg in luci-app-statistics luci-i18n-statistics-ru collectd collectd-mod-conntrack \
-collectd-mod-cpu collectd-mod-cpufreq collectd-mod-entropy collectd-mod-interface \
-collectd-mod-iwinfo collectd-mod-load collectd-mod-memory collectd-mod-network \
-collectd-mod-ping collectd-mod-rrdtool collectd-mod-thermal collectd-mod-uptime \
-prometheus-node-exporter-lua prometheus-node-exporter-lua-nat_traffic \
-prometheus-node-exporter-lua-netstat prometheus-node-exporter-lua-openwrt \
-prometheus-node-exporter-lua-wifi; do
+# --- 7. VPN И ОБХОД БЛОКИРОВОК ---
+# Sing-box (ядро для многих протоколов)
+echo "CONFIG_PACKAGE_sing-box=y" >> .config
+
+# AmneziaWG (протокол и интерфейс)
+for pkg in kmod-amneziawg amneziawg-tools luci-proto-amneziawg luci-i18n-amneziawg-ru; do
     echo "CONFIG_PACKAGE_$pkg=y" >> .config
 done
 
-# 4. AmneziaWG и Podkop
-for pkg in podkop luci-app-podkop luci-i18n-podkop-ru \
-amneziawg-tools kmod-amneziawg luci-proto-amneziawg luci-i18n-amneziawg-ru; do
+# Podkop (точечный обход блокировок)
+for pkg in podkop luci-app-podkop luci-i18n-podkop-ru; do
     echo "CONFIG_PACKAGE_$pkg=y" >> .config
 done
