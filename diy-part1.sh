3626237e1135dcaee7368c65a5f9da9b7b0d52c3
nikita69tv <71224421+nikita69tv@users.noreply.github.com>
2026-04-10 09:00:55 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git podkop https://github.com/itdoginfo/podkop-openwrt.git' >> feeds.conf.default
+echo 'src-git podkop https://github.com/itdoginfo/podkop.git' >> feeds.conf.default
 
